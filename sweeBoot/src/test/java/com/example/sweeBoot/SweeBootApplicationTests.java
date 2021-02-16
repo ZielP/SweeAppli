54b6a7b93666037d72cf132a6e430662f69af36b
ZielP <piotr.zielinski@student.pk.edu.pl>
2021-02-16 17:25:58 +0100

    Spring boot initial commit

---
+++ sweeBoot/src/test/java/com/example/sweeBoot/SweeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweeBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
