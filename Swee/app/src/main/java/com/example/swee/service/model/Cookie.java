c8f2d80fbaee11905365b9638df7be3334d26e55
ZielP <piotr.zielinski@student.pk.edu.pl>
2021-03-02 13:06:43 +0100

    Swee project setup

---
+++ Swee/app/src/main/java/com/example/swee/service/model/Cookie.java
@@ -0,0 +1,10 @@
+package com.example.swee.service.model;
+
+import java.util.List;
+
+public class Cookie {
+    private Long id;
+    private String name;
+    private List<String> ingredients;
+    private String recipe;
+}
---
+++ Swee/app/src/main/java/com/example/swee/view/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.swee.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.swee.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
